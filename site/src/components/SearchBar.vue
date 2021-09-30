5e9ecea007123b779931c49d7679a9a967217fdd
centipeda <joshua.c.guam@gmail.com>
2021-09-30 15:07:10 -0400

    disable button, rebuild

--- site/src/components/SearchBar.vue
+++ site/src/components/SearchBar.vue
@@ -4,7 +4,10 @@
         <input class="search" id="search-text" type="text" placeholder="ex. ranger 1st-level conjuration"
         @input="$emit('update:query', $event.target.value)" :value="query">
     </div>
-    <a target="_blank" class="w-40 f5 link br-pill pv2 mt2 mb2 dib get-btn" @click="getSpellCards" :href="'/spellcard?cards='+ selection.join(',')">
+    <a v-if="selection.length > 0" target="_blank" class="w-40 f5 link br-pill pv2 mt2 mb2 dib get-btn-active" @click="getSpellCards" :href="'/spellcard?cards='+ selection.join(',')">
+        Get selected as printable spell cards
+    </a>
+    <a v-else class="w-40 f5 link br-pill pv2 mt2 mb2 dib get-btn" @click="getSpellCards" disabled="disabled">
         Get selected as printable spell cards
     </a>
 </template>
@@ -43,6 +46,12 @@ export default {
         width: 25%;
     }
     .get-btn {
+        background-color: gray;
+        color: black;
+        font-family: 'ScalaSansRegular';
+        cursor: not-allowed;
+    }
+    .get-btn-active {
         background-color: #add8e6;
         color: black;
         font-family: 'ScalaSansRegular';
